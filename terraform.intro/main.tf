5b9ab95e46e6fcb2b8aaa7f4bf05a380d528a3d2
uzmaa <uzmaashfaq.shavsi@gmail.com>
2025-02-25 11:40:37 +0530

    This is my first terraform file.

--- terraform.intro/main.tf
+++ terraform.intro/main.tf
@@ -4,3 +4,18 @@ resource "local_file" "foo" {
   content  = "hello world"
   filename = "tmp/foo.bar"
 }
+
+
+variable "x" {
+  default = 10
+}
+
+
+
+output "x" {
+  value = var.x
+}
+
+output "x1" {
+  value = "value of x - ${var.x}"
+}
