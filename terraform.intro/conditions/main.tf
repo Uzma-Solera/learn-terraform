1a74138bb1b3d22c44ce98e036d156b34e6ad27c
uzmaa <uzmaashfaq.shavsi@gmail.com>
2025-03-02 00:53:57 +0530

    This is my first terraform file.

--- terraform.intro/conditions/main.tf
+++ terraform.intro/conditions/main.tf
@@ -1,7 +1,12 @@
+
 resource "local_file" "example" {
   content = var.content == null ? "null content" : var.content
   filename = "/tmp/a.txt"
 }
 variable "content" {
   default = null
+}
+
+resource "null_resource" "example" {
+  count = var.content == "null" ? 0 : 1
 }
